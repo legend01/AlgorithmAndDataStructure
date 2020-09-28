d58f8999a89ffa0462f8d76202624f9de7774f5d
hlli8 <lg95312lg@outlook.com>
2020-09-28 14:58:33 +0800

    [增加代码]1.增加链表的操作函数

--- QuickSort/queue.c
+++ QuickSort/queue.c
@@ -1,10 +1,46 @@
+/*
+ * @Description:链表
+ * @version: 
+ * @Author: HLLI8
+ * @Date: 2020-09-28 14:46:27
+ * @LastEditors: HLLI8
+ * @LastEditTime: 2020-09-28 14:57:47
+ */
 #include "queue.h"
+#include <stdlib.h>
 
+LNode *InitLNode(void){
+    LNode *head = (LNode *)malloc(sizeof(LNode));
+    if(head == NULL){
+        return -1;
+    }
+    head->data = NULL;
+    head->next = NULL;
+    return head;
+}
 
+int InsertLNode(LNode *head, int x){
+    LNode *p = NULL, *r = head;
+    while (r->next != NULL)
+    {
+        r = r->next;
+    }
+    p = (LNode *)malloc(sizeof(LNode));
+    p->next = NULL;
 
+    p->data = x;
+    r->next = p;
+    return 1;
+}
 
-
-
+void ShowLNode(LNode *head){
+    LNode *p = head->next;
+    while (p != NULL)
+    {
+        printf("[INFO] LNode->data: %d\n", p->data);
+        p = p->next;
+    }
+}
 
 
 
--- QuickSort/queue.h
+++ QuickSort/queue.h
@@ -1,5 +1,11 @@
 #ifndef __QUEUE_H__
 #define __QUEUE_H__
+typedef struct LNode{
+    int data;
+    struct Lnode *next;
+}LNode;
 
-
+LNode *InitLNode(void);
+int InsertLNode(LNode *head, int x);
+void ShowLNode(LNode *head);
 #endif // !__QUEUE_H__
