3dfa033a40953d825d12b4f0c7627cf2a3c833d7
hlli8 <lg95312lg@outlook.com>
2020-10-12 09:50:52 +0800

    [提交代码]1.将两个数组中的数据查找公共元素，加入到链表中，显示数据

--- QuickSort/ArrayIntersection.c
+++ QuickSort/ArrayIntersection.c
@@ -1,20 +1,26 @@
 #include "ArrayIntersection.h"
+#include "queue.h"
 
 void ArrayIntersection(int *arr1, int size1, int *arr2, int size2) {
-    int count = 0;
-    if(size1 <= size2) {
-        for (int i = 0; i < size1; i++)
-        {
-            for (int j = 0; j < size2; j++)
-            {
-                if(arr1[i] == arr2[j]){
-                    
-                }
-            }
-            
-        }
-        
+    static int count = 0;
+    LNode *head = InitLNode();
+    int *arr_temp;
+    if(size1 ==0 || size2 ==0) return;
+    if(size1 > size2) {
+        arr_temp = arr1;
+        arr1 = arr2;
+        arr2 = arr_temp;
     }
+    for (int i = 0; i < size1; i++){
+        for (int j = 0; j < size2; j++){
+            if(arr1[i] == arr2[j]){
+                InsertLNode(head, arr1[i]);
+                count++;
+            }
+        }
+    }
+    printf("[INFO] the data number: %d \n", count);
+    ShowLNode(head);
 }
 
 
--- QuickSort/main.c
+++ QuickSort/main.c
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: HLLI8
+ * @Date: 2020-09-25 14:03:55
+ * @LastEditors: HLLI8
+ * @LastEditTime: 2020-09-29 17:30:08
+ */
 #include <stdio.h>
 #include "QuickSort.h"
 #include "showArray.h"
@@ -24,5 +32,6 @@ int main(int argc, char *argv[]){
     #endif
     showArrayInf(Array1, ArraySize(Array1), GETNAME(Array1));
     showArrayInf(Array2, ArraySize(Array2), GETNAME(Array2));
+    ArrayIntersection(Array1, ArraySize(Array1), Array2, ArraySize(Array2));
     return 0;
 }
