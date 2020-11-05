ee90f9ceef93f8b48ca2d8cd899352e3c97c6c65
happyCoding <lg95312lg@outlook.com>
2020-11-05 15:56:16 +0800

    打印数组

--- Rotate_Array/Rotate_Array/message.c
+++ Rotate_Array/Rotate_Array/message.c
@@ -2,16 +2,17 @@
  * @Description: 
  * @Author: HLLI8
  * @Date: 2020-11-05 15:27:51
- * @LastEditTime: 2020-11-05 15:32:43
+ * @LastEditTime: 2020-11-05 15:43:38
  * @LastEditors: HLLI8
  */
+#include <stdio.h>
 #include "message.h"
 
 void Inf_Log(int *array, int array_size,char *array_name){
     printf("[INFO] %s data is:", array_name);
     for (int i = 0; i < array_size; i++)
     {
-        printf("%d ", array)
+        printf("%d ", array[i]);
     }
     printf("\n");
 }
