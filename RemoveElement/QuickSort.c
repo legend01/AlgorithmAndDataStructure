e34ac0879b1f8caf6ba128c6d8d7873d06f41266
happyCoding <lg95312lg@outlook.com>
2020-11-08 17:20:15 +0800

    快速排序 时间复杂度 空间复杂度

--- RemoveElement/QuickSort.c
+++ RemoveElement/QuickSort.c
@@ -2,11 +2,17 @@
  * @Description: 快速排序
  * @Author: HLLI8
  * @Date: 2020-11-08 17:09:00
- * @LastEditTime: 2020-11-08 17:10:55
+ * @LastEditTime: 2020-11-08 17:17:54
  * @LastEditors: HLLI8
  */
 #include "QuickSort.h"
 
+/**
+ * @description: 快速排序
+ * @Note：
+ * 时间复杂度：O(n^2)
+ * 空间复杂度：O(log2n)
+ */
 void QuickSort(int arr[], int low, int high) {
     int first = low;
     int last = high;
