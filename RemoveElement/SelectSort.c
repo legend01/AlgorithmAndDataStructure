6c8ca6b9f779dab9cf9d8cde866c4db3a5bdb0c3
happyCoding <lg95312lg@outlook.com>
2020-11-08 18:24:37 +0800

    简单选择排序

---
+++ RemoveElement/SelectSort.c
@@ -0,0 +1,31 @@
+/*
+ * @Description: 简单选择排序
+ * @Author: HLLI8
+ * @Date: 2020-11-08 18:04:38
+ * @LastEditTime: 2020-11-08 18:11:07
+ * @LastEditors: HLLI8
+ */
+#include "SelectSort.h"
+
+void SelectSort(int Arr[], int array_size){
+    int i,j,k;
+    int temp;
+    for ( i = 0; i < array_size; i++)
+    {
+        k = i;
+        /* 从无序序列中挑出一个最小的关键字 */
+        for ( j = i+1; j < array_size; j++)
+        {
+            if (Arr[k] > Arr[j]){
+                k = j;
+            }
+        }
+        /* 最小关键字与无序序列第一个关键字交换 */        
+        temp = Arr[i];
+        Arr[i] = R[k];
+        R[k] = temp;
+    }
+    
+}
+
+
---
+++ RemoveElement/SelectSort.h
@@ -0,0 +1,19 @@
+/*
+ * @Description: 简单选择排序
+ * @Author: HLLI8
+ * @Date: 2020-11-08 18:04:17
+ * @LastEditTime: 2020-11-08 18:05:44
+ * @LastEditors: HLLI8
+ */
+#ifndef __SELECTSORT_H__
+#define __SELECTSORT_H__
+
+
+#endif // !__SELECTSORT_H__
+
+
+
+
+
+
+
